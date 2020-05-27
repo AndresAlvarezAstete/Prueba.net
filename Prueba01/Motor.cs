f220396a83b2990786899d9571c5a366a95c268d
Andrés Álvarez Astete <andres.alvarez.as@gmail.com>
2020-05-27 19:32:15 -0400

    Upload

---
+++ Prueba01/Motor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prueba01
+{
+    enum TipoMotor
+    {
+        DOS_TIEMPOS, CUATRO_TIEMPOS
+    }
+    class Motor : VehiculoComponente 
+    {
+        private readonly string _id;
+        private TipoMotor _tipoMotor;
+        private int _cilindrada;
+
+        public Motor(string id, TipoMotor tipoMotor, int cilindrada)
+        {
+            _id = id;
+            _tipoMotor = tipoMotor;
+            _cilindrada = cilindrada;
+        }
+
+        public string Id => _id;
+        public int Cilindrada { get => _cilindrada; set => _cilindrada = value; }
+        internal TipoMotor TipoMotor { get => _tipoMotor; set => _tipoMotor = value; }
+    }
+}
---
+++ Prueba01/Vehiculo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prueba01
+{
+    abstract class Vehiculo
+    {
+        // private = modificador de acceso
+        // public
+        // protected
+        private Motor _motor;
+
+        protected Vehiculo(string id, TipoMotor tipoMotor, int cilindrada)
+        {
+            _motor = new Motor(id, tipoMotor, cilindrada);
+        }
+
+        internal Motor Motor { get => _motor; set => _motor = value; }
+    }
+}
